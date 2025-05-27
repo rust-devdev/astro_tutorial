53ccb14809a19f6168aec42d78ff73cb75b00127
yumi <rust.devdev@gmail.com>
2025-05-27 21:43:32 +0900

    ハイドレートされた（hydrated） コンポーネントの作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-tutorial-dev.netlify.app/"
+  site: "https://astro-tutorial-dev.netlify.app/",
+  integrations: [preact()]
 });
